f6425bbc388882f31450acc289340d6442864714
DerEmox <emil.schuster.5@web.de>
2025-03-12 21:33:46 +0100

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+  import { Transaction } from "./lib/transaction";
+  let t = new Transaction("Mark", "Uwe", 0.5)
+</script>
+{@html t.outputHTML()}
+<style>
+  
+</style>
---
+++ src/lib/transaction.ts
@@ -0,0 +1,17 @@
+export class Transaction {
+    private sourceWalletID: string
+    private targetWalletID: string
+    private value: number
+    private fee: number = Math.round((Math.random()*100)) / 1000
+
+    constructor(sourceWalletID: string, targetWalletID: string, value: number) {
+        this.sourceWalletID = sourceWalletID
+        this.targetWalletID = targetWalletID
+        this.value = value
+    }
+
+    public outputHTML(): string {
+        let html = `<p>Transaction from <span style="color: red">${this.sourceWalletID}</span> to <span style="color: red">${this.targetWalletID}</span> with a value of <span style="color: lightblue">${this.value}B</span> and a fee of <span style="color: lightblue">${this.fee}B</span></p>`
+        return html
+    }
+}
